b64302a935c28e191906574966642af9c4a0cc1f
bitwilygame <bitwilygame@gmail.com>
2025-12-19 18:09:32 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Auto-generated / local-only Firebase config for static hosting.
+// This file is loaded by `index.html` to populate `window.__FIREBASE_CONFIG__`.
+// NOTE: Firebase web config values are not considered secret, but keep your Auth rules secure.
+
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyBW-_MbzdMLmoRGJaaMPSAMg6LG8oEAdMg",
+  authDomain: "studio-945632983-48c2e.firebaseapp.com",
+  projectId: "studio-945632983-48c2e",
+  databaseURL: "https://studio-945632983-48c2e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  storageBucket: "",
+  messagingSenderId: "726923989176",
+  appId: "1:726923989176:web:731c18b9b90bbe4f2a644d",
+  measurementId: "",
+};
